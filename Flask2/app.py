3270ef2bc799ff3b66b883cc31209de94bb09be3
ZEFERUS777 <pomagomeddobri@gmail.com>
2025-02-27 23:06:11 +0300

    complete

---
+++ Flask2/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+@app.route('/login')
+def login():
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
