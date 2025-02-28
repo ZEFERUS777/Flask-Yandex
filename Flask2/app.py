028690d55c197989aa5a73ff7f874ce4dd96c82a
ZEFERUS777 <pomagomeddobri@gmail.com>
2025-02-28 14:00:27 +0300

    complete task

--- Flask2/app.py
+++ Flask2/app.py
@@ -33,5 +33,11 @@ def prof(list_type):
                            professions=professions)
 
 
+@app.route('/login')
+def login_form():
+    return render_template('form.html')
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
